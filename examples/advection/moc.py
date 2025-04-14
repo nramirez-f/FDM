aefa333284396c8e3eab722b1d96e7d47b46050d
Nramirez <nramirez@uma.es>
2025-04-14 14:38:40 +0200

    build(examples):create examples for method of characteristics

--- examples/advection/moc.py
+++ examples/advection/moc.py
@@ -1,5 +1,5 @@
 from advection import method_of_characteristics as moc
-from netCDFviewer import NCV
+from ncviewer import NcFile
 from initial_conditions import *
 
 x0 = -4
@@ -7,10 +7,9 @@ xf = 4
 nx = 400
 T = 10
 nt = 40
-c = -0.5
+c = 0.5
 
 filepath = moc(x0, xf, nx, T, nt, c, f3)
 
-ncv = NCV(filepath)
-ncv.shape(5)
-ncv.close()
+ncv = NcFile(filepath)
+ncv.evolution(0)
--- examples/burgers/pmoc.py
+++ examples/burgers/pmoc.py
@@ -1,12 +1,10 @@
 from burgers import plot_method_of_characteristics as pmoc
 from initial_conditions import *
-import numpy as np
 
 x0 = -6
 xf = 6
 nx = 400
 T = 10
 nt = 40
-c = -0.5
 
 pmoc(x0, xf, nx, T, nt, f1)
